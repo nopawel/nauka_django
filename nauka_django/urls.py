6a9bcb08ad5ddafeedc9351b1ed629bb680fa7ae
nopawel <nopawel@wp.pl>
2013-08-12 00:06:05 +0200

    konfiguracja bazy danych

--- nauka_django/urls.py
+++ nauka_django/urls.py
@@ -5,6 +5,5 @@ urlpatterns = patterns('',
                        url(r'^hello/$', hello),
                        url(r'^$', strona_glowna),
                        url(r'^time/$', current_datetime),
-                       url(r'^time/plus/\d+/$', hours_ahead),
-                       url(r'^time/plus_update/(\d{1,2})/$', hours_ahead),
+                       url(r'^time/plus/(\d{1,2})/$', hours_ahead),
 )
