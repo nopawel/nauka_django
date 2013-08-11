0235658b5f0ab16d54f5ca776360061f19ec5f08
nopawel <nopawel@wp.pl>
2013-08-11 19:30:06 +0200

    poprawka wpisu

--- nauka_django/urls.py
+++ nauka_django/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from nauka_django.views import hello, strona glowna
+from nauka_django.views import hello, strona_glowna
 
 urlpatterns = patterns('',
                        url(r'^hello/$', hello),
