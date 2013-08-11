0ed6cb37b7027dbb4d8d621074f612aaff5be643
nopawel <nopawel@wp.pl>
2013-08-11 20:16:06 +0200

    Porownanie dwoch podstron automatycznych

--- nauka_django/urls.py
+++ nauka_django/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
                        url(r'^$', strona_glowna),
                        url(r'^time/$', current_datetime),
                        url(r'^time/plus/\d+/$', hours_ahead),
+                       url(r'^time/plus_update/(\d{1,2})/$', hours_ahead),
 )
