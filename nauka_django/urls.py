33e956fcdac753f74fdd33be4ad56df6dd620b4a
nopawel <nopawel@wp.pl>
2013-08-11 19:27:59 +0200

    dodałem strone główna

--- nauka_django/urls.py
+++ nauka_django/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
-from nauka_django.views import hello
+from nauka_django.views import hello, strona glowna
 
 urlpatterns = patterns('',
                        url(r'^hello/$', hello),
+                       url(r'^$', strona_glowna),
    
 )
--- nauka_django/views.py
+++ nauka_django/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def hello(request):
     return HttpResponse("Hello world")
+
+def strona_glowna(request):
+    return HttpResponse("To jest strona główna moejgo pierwszego projektu Django")
