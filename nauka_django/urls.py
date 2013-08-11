dafe56382445977e8f55bf33d8ccf3822bcbfd5c
nopawel <nopawel@wp.pl>
2013-08-11 20:21:53 +0200

    dodanie funkcji hours_ahead

--- nauka_django/urls.py
+++ nauka_django/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from nauka_django.views import hello, strona_glowna, current_datetime
+from nauka_django.views import hello, strona_glowna, current_datetime, hours_ahead
 
 urlpatterns = patterns('',
                        url(r'^hello/$', hello),
--- nauka_django/views.py
+++ nauka_django/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 import datetime
 
 def hello(request):
@@ -10,4 +10,13 @@ def strona_glowna(request):
 def current_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body>Taka mamy godzine %s.</body></html>" % now
+    return HttpResponse(html)
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
     return HttpResponse(html)
