16c7048bbaf37c9c1407f914166a210f065d112e
nopawel <nopawel@wp.pl>
2013-08-11 19:53:19 +0200

    Dodałem podstrone time i funkcje czasu

--- nauka_django/urls.py
+++ nauka_django/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
-from nauka_django.views import hello, strona_glowna
+from nauka_django.views import hello, strona_glowna, current_datetime
 
 urlpatterns = patterns('',
                        url(r'^hello/$', hello),
                        url(r'^$', strona_glowna),
+                       url(r'^time/$', current_datetime),
    
 )
