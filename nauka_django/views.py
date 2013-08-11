9eaa0a6fa42cf6f04bcdff6950293d10325e951b
nopawel <nopawel@wp.pl>
2013-08-11 19:44:54 +0200

    Dodałem funkcje czasu

--- nauka_django/views.py
+++ nauka_django/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
+import datetime
 
 def hello(request):
     return HttpResponse("Hello world")
 
 def strona_glowna(request):
     return HttpResponse("To jest strona glowna mojego pierwszego projektu Django")
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>Taka mamy godzine %s.</body></html>" % now
+    return HttpResponse(html)
