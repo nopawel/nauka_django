f16e74c010ef3955e7c8e5862e1e92f9b37a117f
nopawel <nopawel@wp.pl>
2013-08-11 19:11:04 +0200

    Usunałem podpowiedzi, dodałem wpis

--- nauka_django/urls.py
+++ nauka_django/urls.py
@@ -1,18 +1,7 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from nauka_django.views import hello
 
 urlpatterns = patterns('',
                        url(r'^hello/$', hello),
-    # Examples:
-    # url(r'^$', 'nauka_django.views.home', name='home'),
-    # url(r'^nauka_django/', include('nauka_django.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+   
 )
