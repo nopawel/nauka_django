381b479d893d22a5ba173c16b7d8ace7a0a2b72d
nopawel <nopawel@wp.pl>
2013-08-12 22:28:36 +0200

    Dodanie panelu admina

--- nauka_django/urls.py
+++ nauka_django/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import patterns, include, url
 from nauka_django.views import hello, strona_glowna, current_datetime, hours_ahead
+from django.contrib import admin
+admin.autodiscover()
+
 
 urlpatterns = patterns('',
                        url(r'^hello/$', hello),
                        url(r'^$', strona_glowna),
                        url(r'^time/$', current_datetime),
                        url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+                       (r'^admin/', include(admin.site.urls)),
 )
