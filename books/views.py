3b28b5858a019e3167d89ba6d7da5d8a3658531c
nopawel <nopawel@wp.pl>
2013-08-17 20:37:07 +0200

    Nauka strona do szukania

--- books/views.py
+++ books/views.py
@@ -1,1 +1,5 @@
 # Create your views here.
+from django.shortcuts import render
+
+def search_form(request):
+    return render(request, 'search_form.html')
--- nauka_django/urls.py
+++ nauka_django/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from nauka_django.views import hello, strona_glowna, current_datetime, hours_ahead,display_meta
+from nauka_django.views import hello, strona_glowna, current_datetime, hours_ahead,display_meta, views
 from django.contrib import admin
 admin.autodiscover()
 
@@ -10,6 +10,8 @@ urlpatterns = patterns('',
                        url(r'^$', strona_glowna),
                        url(r'^time/$', current_datetime),
                        url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+                       url(r'^search-form/$', views.search_form),
                        (r'^admin/', include(admin.site.urls)),
+                       
                        
 )
