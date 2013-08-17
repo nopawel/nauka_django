2dd81bf7d2e781fd9cc08a5b1bdf4fe900c81049
nopawel <nopawel@wp.pl>
2013-08-17 20:25:45 +0200

    Formsy - nauka

--- nauka_django/urls.py
+++ nauka_django/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import patterns, include, url
-from nauka_django.views import hello, strona_glowna, current_datetime, hours_ahead
+from nauka_django.views import hello, strona_glowna, current_datetime, hours_ahead,display_meta
 from django.contrib import admin
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
                        url(r'^hello/$', hello),
+                       url(r'^display_meta/$',display_meta),
                        url(r'^$', strona_glowna),
                        url(r'^time/$', current_datetime),
                        url(r'^time/plus/(\d{1,2})/$', hours_ahead),
                        (r'^admin/', include(admin.site.urls)),
+                       
 )
--- nauka_django/views.py
+++ nauka_django/views.py
@@ -4,6 +4,14 @@ import datetime
 def hello(request):
     return HttpResponse("Hello world")
 
+def display_meta(request):
+    values = request.META.items()
+    values.sort()
+    html = []
+    for k, v in values:
+        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
+    return HttpResponse('<table>%s</table>' % '\n'.join(html))
+
 def strona_glowna(request):
     return HttpResponse("To jest strona glowna mojego pierwszego projektu Django")
 
